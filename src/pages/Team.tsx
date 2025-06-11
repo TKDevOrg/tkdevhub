124e1c09095afe190391a896f9fe96d4c93b258f
Raja B <138191971+raja-balaji@users.noreply.github.com>
2025-06-11 13:49:40 -0500

    Team Page Update

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -8,17 +8,15 @@ const teamMembers = [
   {
     name: 'Eugene Vincent',
     role: 'Co-Founder & Software Engineer',
-    image: 'https://media.licdn.com/dms/image/v2/D5603AQFgqsId7vSMdw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726194444193?e=1749081600&v=beta&t=yr_Xc8a3C-J2NQf3VfTsZ8XQs528uirFal1u2ki37r4',
+    image: 'https://media.licdn.com/dms/image/v2/D5603AQFgqsId7vSMdw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1726194444166?e=2147483647&v=beta&t=wwfO5w9owFt-dayRGoHgT31kxWTrgjFBiLkhHyPi_IU',
     initials: 'EV',
-    github: 'https://github.com/eugenevincent', 
     linkedin: 'https://www.linkedin.com/in/eugene-vincent-42472024b/', 
   },
   {
     name: 'Raja Balaji',
     role: 'Co-Founder & Software Engineer',
-    image: 'https://media.licdn.com/dms/image/v2/D5603AQFjRg1v6RNkhg/profile-displayphoto-shrink_200_200/B56ZSNv6DCGoAc-/0/1737544934002?e=1749081600&v=beta&t=PZSwGF7i9QN5hKw1ujxvrsSZA60zkqp-8fP4e7NmjJE',
+    image: 'https://media.licdn.com/dms/image/v2/D5603AQES0vbWO9PTnw/profile-displayphoto-shrink_800_800/B56ZY3BNF5H0Ac-/0/1744679785154?e=1755129600&v=beta&t=HOuvptpa5e-Z2AU48hsj8-r6SbVm5idERl_QC4nKQxY',
     initials: 'RB',
-    github: 'https://github.com/rajabalaji', 
     linkedin: 'https://www.linkedin.com/in/rajabalaji/', 
   },
   {
@@ -26,7 +24,6 @@ const teamMembers = [
     role: 'Software Engineer',
     image: 'https://media.licdn.com/dms/image/v2/D5603AQE46pHjyCpstQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1730052479397?e=1749081600&v=beta&t=xXt_VYz3dVCo8JaHSlMuzfU-VYrv9nAufT-04xaqodw',
     initials: 'MJ',
-    github: 'https://github.com/maksatjanjorayev', 
     linkedin: 'https://www.linkedin.com/in/maksatjan-jorayev-6327b8261/', 
   }
 ]
